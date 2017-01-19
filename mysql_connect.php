1dace71f8cb4cb859fad6c8826a56661e898a203
user.name <fi9487@wayne.edu>
2017-01-19 15:30:27 -0500

    Added more comments

--- mysql_connect.php
+++ mysql_connect.php
@@ -1,10 +1,12 @@
 <?php
 
+	//Change the username and password fields if using different credentials for your database. The other fields may be changed if applicable.
 	$username="clientuser";
 	$password="lexluthor";
 	$databaseHost="localhost";
 	$database="tasklist";	
 
+	//Main function used to connect to the database
 	$databaseConnect = @mysqli_connect($databaseHost, $username, $password, $database)
 	OR die('Could not connect to database' . mysqli_connect_error());
 
